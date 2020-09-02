61217875ce44c1bc53ea32a2eae9cb9bbc9b7b0d
Anneka Curry <69550392+nekacurry@users.noreply.github.com>
2020-09-01 19:02:35 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,42 @@
+
+
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+answer = area(10, 10)
+solution = area(4, 7)
+
+print(result)
+print(answer)
+print(solution)
+
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(10, 5)
+answer = subtract(100, 50)
+solution = subtract(188472, 5033)
+
+print(result)
+print(answer)
+print(solution)
+
+
+def divide(num1,num2):
+    result = num1 / num2
+    return result
+    
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+result = add(50,50)
+answer = add(59930,94820)
+
+print(result)
+print(answer)
